615b7530443a6a8e3aab92f4455e306482cf16df
DriverPortalDev <driverportaldev@gmail.com>
2019-07-14 20:37:40 +0000

    blank json

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ class ThingsResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = 'hello?'
+        resp.body = '{"hello":"world"}'
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
