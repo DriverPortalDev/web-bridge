29cc3151004c7f04b4887ced63a2d3615f4e0a45
DriverPortalDev <driverportaldev@gmail.com>
2019-07-14 20:32:31 +0000

    going falcon

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
-from japronto import Application
+import falcon
 
-def hello(request):
-    return request.Response(text='Hello world!')
 
-app = Application()
+class ThingsResource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = 'hello?'
 
-app.router.add_route('/', hello)
+# falcon.API instances are callable WSGI apps
+app = falcon.API()
 
-#app.run(debug=True,host='127.0.0.1', port=3000)
+# Resources are represented by long-lived class instances
+things = ThingsResource()
+
+# things will handle all requests to the '/' URL path
+app.add_route('/', things)
