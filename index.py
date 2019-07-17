b1c0c20f85d9869aa51e24595d6880a8901e6c9d
DriverPortalDev <driverportaldev@gmail.com>
2019-07-17 18:54:16 +0000

    testing stuff

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import falcon
+import os
+import json
 
 
 class ThingsResource(object):
@@ -6,6 +8,11 @@ class ThingsResource(object):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
         resp.body = '{"hello":"world"}'
+
+class TestStuff(object):
+    def on_get(self, req, resp):
+        get_os_value = json.dumps(str(os.environ))
+        resp.body = get_os_value
 
 
 # falcon.API instances are callable WSGI apps
@@ -16,3 +23,4 @@ things = ThingsResource()
 
 # things will handle all requests to the '/' URL path
 app.add_route('/', things)
+app.add_route('/test_stuff', TestStuff())
